86e2590195622ea542ec6c75648da7eac00a530c
isehou <zaku47shift@gmail.com>
2024-04-12 23:40:25 +0600

    fixed bugs, added adaptive

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let passwordEyeHidden = document.querySelector(".password-eye-btn img");
 function toggleHiddenPassword() {
   if (passwordInput.type === "password") {
     passwordInput.type = "text";
-    passwordEyeHidden.src = " ./assets/eye-hide.png";
+    passwordEyeHidden.src = " ./assets/eye-hide.svg";
   } else {
     passwordInput.type = "password";
-    passwordEyeHidden.src = "./assets/eye.png";
+    passwordEyeHidden.src = "./assets/eye.svg";
   }
 }
